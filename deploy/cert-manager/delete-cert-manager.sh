482599a9ab7371f3b27eafbf083df069444a6fb2
Vladislav Klimenko <sunsingerus@gmail.com>
2020-04-23 22:49:48 +0300

    env: clean unsused deployments

--- deploy/cert-manager/delete-cert-manager.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Read this"
-echo "https://cert-manager.io/docs/installation/uninstall/kubernetes/"
--- deploy/helm/install-helm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
